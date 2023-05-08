7971c014e4dadbbb0c918eb8c3dfee71028a58d0
Jake Stanley <prudentia713@gmail.com>
2023-05-08 18:34:45 +0100

    PowerShell: cross platform support, support for running without OBS, limited output without -Debug flag - replaces Out-GridView with an external call to simple-term-menu on non-Windows platforms - if NoObs switch is on, obs client will be null. further calls to obs client use null conditional - use Join-Path for multi platform path compatibility -

--- start.ps1
+++ start.ps1
@@ -1,16 +1,29 @@
+#!/usr/bin/env pwsh
 Param(
     [switch] [Parameter(HelpMessage="OBS control will be disabled")] $NoObs,
     [switch] [Parameter(HelpMessage="Re-record a completed demo")] $ReRecord,
     [switch] [Parameter(HelpMessage="play a random map")] $Random,
     [switch] [Parameter(HelpMessage="Demo recording will be disabled")] $NoDemo,
-    [string] [Parameter(HelpMessage="Override source port")] $SourcePort
+    [string] [Parameter(HelpMessage="Override source port")] $SourcePort,
+    [string] [Parameter(HelpMessage="Path to configuration file")] $ConfigPath = ".\config.json",
+    [string] [Parameter(HelpMessage="Path to maps CSV file")] $CsvFilePath = ".\Season1.csv"
 )
 
 Import-Module OBSWebSocket
 
-$json = Get-Content .\config.json -Raw
+# declare functions here
+function GetMapNameString {
+    param (
+        $Map
+    )
+    return '"#{0}: {1} | {2} | {3}"' -f $map.Ranking, $map.Title, $map.Author, $map.Map
+}
+
+# script begins in earnest
+$json = Get-Content $ConfigPath -Raw
 $config = ConvertFrom-Json $json
 
+# TODO: should use source port configured comp level if this is not set
 $default_complevel=$config.default_complevel
 $dsda_path=$config.dsda_path
 $chocolatedoom_path=$config.chocolatedoom_path
@@ -21,42 +34,57 @@ $pwad_dir=$config.pwad_dir
 $demo_dir=$config.demo_dir
 
 if ($NoObs) {
-    Write-Host "No Obs selected"
-} else {
-    Write-Host "OBS will be controlled"
+    Write-Host "OBS will NOT be controlled"
 }
 
 if ($SourcePort) {
-    Write-Host "Using source port "$SourcePort
+    Write-Host "Overriding with source port '{}'" -f $SourcePort
 }
 
-Write-Host "Configuration:"
-Write-Host "`tdefault_complevel: $default_complevel"
-Write-Host "`tdsda_path: $dsda_path"
-Write-Host "`tchocolatedoom_path: $chocolatedoom_path"
-Write-Host "`tchocolatedoom_cfg_default: $chocolatedoom_cfg_default"
-Write-Host "`tchocolatedoom_cfg_extra: $chocolatedoom_cfg_extra"
-Write-Host "`tiwad_dir: $iwad_dir"
-Write-Host "`tpwad_dir: $pwad_dir"
-Write-Host "`tdemo_dir: $demo_dir"
-
-$csvFilePath = ".\Season1.csv"
+Write-Debug "Configuration:"
+Write-Debug "`tdefault_complevel: $default_complevel"
+Write-Debug "`tdsda_path: $dsda_path"
+Write-Debug "`tchocolatedoom_path: $chocolatedoom_path"
+Write-Debug "`tchocolatedoom_cfg_default: $chocolatedoom_cfg_default"
+Write-Debug "`tchocolatedoom_cfg_extra: $chocolatedoom_cfg_extra"
+Write-Debug "`tiwad_dir: $iwad_dir"
+Write-Debug "`tpwad_dir: $pwad_dir"
+Write-Debug "`tdemo_dir: $demo_dir"
 
 try {
 
     # Connect to OBS and switch to the "waiting" scene
-    $r_client = Get-OBSRequest -hostname "localhost" -port 4455
-    $r_client.SetCurrentProgramScene("Waiting")
+    $r_client = $NoObs ? $null : (Get-OBSRequest -hostname "localhost" -port 4455)
+    ${r_client}?.SetCurrentProgramScene("Waiting")
 
     # load maps from CSV
-    $csvData = Import-Csv $csvFilePath
+    $csvData = Import-Csv $CsvFilePath
 
     $maps = @()
     foreach ($row in $csvData) {
         $maps += $row
     }
 
-    $map = $maps | Out-GridView -Title "Select a map" -OutputMode Single
+    if(!(Get-Command Out-GridView -ErrorAction SilentlyContinue)) {
+        Write-Debug "Out-GridView not supported. Attempting to use simple-term-menu"
+        $options = @()
+        foreach($map in $maps) {
+            $options += GetMapNameString($map)
+        }
+        
+        # use last exit code to obtain selected item
+        & simple-term-menu $options
+        $selected = $LASTEXITCODE-1
+        if ($selected -lt 0) {
+            Write-Error "A map was not selected"
+            Exit 1
+        }
+
+        $map = $maps[$selected]
+    } else {
+        $map = $maps | Out-GridView -Title "Select a map" -OutputMode Single
+    }
+
     if (!$map) {
         Write-Error "A map was not selected"
         Exit 1
@@ -64,11 +92,9 @@ try {
 
     # default arguments
     $complevel = $map.CompLevel -replace '^$', $default_complevel
-    $args = "-nomusic -skill 4 "
-
-    # default
+    $args = [System.Collections.ArrayList]::new()
+    $args.AddRange(@("-nomusic", "-skill", 4))
     $demo_prefix = ""
-    $demo_name = ""
 
     # detect which IWAD we need and the map id format
     if ($map.Map -match "^E(\d)M(\d)$") {
@@ -76,14 +102,14 @@ try {
         $demo_prefix="DOOM" # default just in case no pwad is provided
         $episodeno = [int]$Matches[1]
         $mapno = [int]$Matches[2]
-        $args += "-warp $episodeno $mapno "
-        $args += "-iwad $iwad_dir\DOOM.WAD "
+        $args.AddRange(@("-warp", $episodeno, $mapno))
+        $args.AddRange(@("-iwad", (Join-Path -Path $iwad_dir -ChildPath "DOOM.WAD")))
     } elseif($map.Map -match "^MAP(\d+)$") {
         Write-Host "Detected a Doom II map string"
         $demo_prefix="DOOM2" # default just in case no pwad is provided
         $mapno = [int]$Matches[1]
-        $args += "-warp $mapno "
-        $args += "-iwad $iwad_dir\DOOM2.WAD "
+        $args.AddRange(@("-warp", $mapno))
+        $args.AddRange(@("-iwad", (Join-Path -Path $iwad_dir -ChildPath "DOOM2.WAD")))
     } else {
         Write-Error "Could not parse Map value: '$map.Map'"
     }
@@ -96,9 +122,9 @@ try {
     foreach($patch in $map.Files.Split("|")) {
         $fileExtension = [System.IO.Path]::GetExtension($patch).ToLower()
         if ($fileExtension -like "*.deh") {
-            $dehs += "$pwad_dir\$patch"
+            $dehs += Join-Path -Path $pwad_dir -ChildPath $patch
         } elseif($fileExtension -like "*.wad") {
-            $pwads += "$pwad_dir\$patch"
+            $pwads += Join-Path -Path $pwad_dir -ChildPath $patch
         } else {
             # ignore file
         }
@@ -106,58 +132,65 @@ try {
 
     # for chocolate doom/vanilla wad merge emulation
     foreach($merge in $map.Merge.Split("|")) {
-        $mwads += "$pwad_dir\$merge"
+        $mwads += Join-Path -Path $pwad_dir -ChildPath $merge
     }
 
     if ($dehs.Count -gt 0) {
-        $args += "-deh $dehs "
+        $args.Add("-deh");
+        $args.AddRange($dehs)
     }
 
     if ($pwads.Count -gt 0) {
         $demo_prefix = [System.IO.Path]::GetFileNameWithoutExtension($pwads[0])
-        $args += "-file $pwads "
+        $args.Add("-file")
+        $args.AddRange($pwads)
     }
 
     # Set map title in OBS
-    $r_client.Call("SetInputSettings", @{
+    ${r_client}?.Call("SetInputSettings", @{
         inputName = "Text"
         inputSettings = @{
             text = $map.Title + " - " + $map.Author
         }
     })
 
-    # record the demo
-    $time= (Get-Date).ToString("yyyy-MM-ddTHmmss")
-    $arg_record = "-record " + "$demo_dir" + "\" + $demo_prefix + "-" + $map.Map + "-" + "$time" + ".lmp "
-    $args += $arg_record
 
-    if ($map.Port -eq "chocolate") {
+    # record the demo
+    # TODO: check demo directory is writeable
+    if (!$NoDemo) {
+        $time= (Get-Date).ToString("yyyy-MM-ddTHmmss")
+        $args.AddRange(@("-record", (Join-Path -Path $demo_dir -ChildPath ("{0}-{1}-{2}.lmp" -f $demo_prefix, $map.Map, $time))))
+    }
+
+    # if the port must be chocolate doom or we have overridden to use chocolate doom (support for more later)
+    if ($map.Port -eq "chocolate" || $SourcePort -eq "chocolate") {
 
         if ($mwads.Count -gt 0) {
-            $args += "-merge $mwads "
+            $args.Add("-merge")
+            $args.AddRange($mwads)
         }
         
-        Write-Host "Starting chocolate-doom with the following arguments:"
-        $args += "-config $chocolatedoom_cfg_default -extraconfig $chocolatedoom_cfg_extra "
+        Write-Debug "Starting chocolate-doom with the following arguments:"
+        $args.AddRange(@("-config", $chocolatedoom_cfg_default, "-extraconfig", $chocolatedoom_cfg_extra)) 
         $executable = $chocolatedoom_path
     } else {
         # default to dsda-doom and set complevel args
-        $args += "-complevel " + $complevel + " -window "
-        Write-Host "Starting dsda-doom with the following arguments:"
+        $args.AddRange(@("-complevel", $complevel, "-window")) 
+        Write-Debug "Starting dsda-doom with the following arguments:"
         $executable = $dsda_path
     }
 
-    Write-Host $args
+    foreach($arg in $args) {
+        Write-Debug `t`t$arg
+    }
     Start-Sleep 3
 
-    $r_client.SetCurrentProgramScene("Playing")
+    ${r_client}?.SetCurrentProgramScene("Playing")
     Start-Process -FilePath $executable -ArgumentList $args -Wait
-    $r_client.SetCurrentProgramScene("Waiting")
-
-    # $resp = $r_client.GetVersion()
-    # Write-Host "obs version:", $resp.obsVersion
-    # Write-Host "websocket version:", $resp.obsWebSocketVersion
+    ${r_client}?.SetCurrentProgramScene("Waiting")
 
 } finally { 
-    $r_client.TearDown() 
+    ${r_client}?.TearDown()
 }
+
+Write-Host "Script exiting"
